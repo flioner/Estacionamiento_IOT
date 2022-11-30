cb6f3547b1c04623b9dcf83e02ed8ba0e67936ff
flioner <lionerrochae@gmail.com>
2022-11-29 21:53:51 -0600

    Heatmap concept

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, onValue, ref } from "firebase/database";
 
 export default function Home() {
-  const [lugares, updateDisponibles] = useState(0);
+  const [lugares, updateLugares] = useState([11]);
 
   const firebaseConfig = {
     apiKey: "",
@@ -14,20 +14,40 @@ export default function Home() {
     databaseURL: ``,
   };
 
+  const UpdateAllLugares = (arr: any, pos: number) => {
+    const newLugares = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i == pos) {
+        newLugares.push(arr[i] + 1);
+      } else {
+        newLugares.push(arr[i]);
+      }
+    }
+    updateLugares(newLugares);
+  };
+
+  const AddLugar = () => {
+    const newLugares = lugares;
+    newLugares.push(0);
+    updateLugares(newLugares);
+  };
   const firebaseApp = initializeApp(firebaseConfig);
   const database = getDatabase(firebaseApp);
 
   useEffect(() => {
-    const starCountRef = ref(database, "number");
+    const starCountRef = ref(database, "e1");
     onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
       console.log(data);
       if (data != null) {
-        updateDisponibles(data);
+        updateLugares([1]);
       }
+      updateLugares([1]);
     });
   }, []);
 
+  // Add Lugar
+  // updateLugares([...lugares, 1])
   return (
     <div className={s.container}>
       <Head>
@@ -39,17 +59,59 @@ export default function Home() {
       <div className={s.mainTitle}>Estacionamiento</div>
       <div className={s.disponibles}>
         <div>Lugares Disponibles:&nbsp;</div>
-        <div>3</div>
+        <div>{lugares[0]}</div>
       </div>
 
       <div className={s.grid}>
-        <div className={s.disponible}>1</div>
-        <div className={lugares == 42 ? s.ocupado : s.disponible}>2</div>
-        <div className={s.disponible}>3</div>
-        <div className={s.disponible}>4</div>
+        <div
+          className={`${s.lugar} ${lugares[0] != 1 ? s.disponible : s.ocupado}`}
+        >
+          1
+        </div>
+        <div
+          className={`${s.lugar} ${lugares[1] != 1 ? s.disponible : s.ocupado}`}
+        >
+          2
+        </div>
+        <div
+          className={`${s.lugar} ${lugares[2] != 1 ? s.disponible : s.ocupado}`}
+        >
+          3
+        </div>
+        <div
+          className={`${s.lugar} ${lugares[3] != 1 ? s.disponible : s.ocupado}`}
+        >
+          4
+        </div>
       </div>
 
-      <div>{lugares}</div>
+      <div className={s.disponibles}>
+        <div onClick={() => AddLugar()}>Lugares más concurridos:</div>
+      </div>
+
+      <div className={s.grid}>
+        {lugares.map((l, i) => (
+          <div
+            key={i}
+            onClick={() => UpdateAllLugares(lugares, i)}
+            className={`${s.lugar} ${
+              lugares[i] <= 3
+                ? s.lime
+                : lugares[i] <= 5
+                ? s.green
+                : lugares[i] <= 7
+                ? s.yellow
+                : lugares[i] <= 9
+                ? s.orange
+                : lugares[i] <= 11
+                ? s.red
+                : s.darkRed
+            }`}
+          >
+            {lugares[i]}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
